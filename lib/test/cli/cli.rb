fe40a06dba94a25b51657d23573a9a2456749448
Angelica  Thompson <i.thompson89@outlook.com>
2020-01-14 06:39:54 +0000

    fixing cli.rb

--- lib/test/cli/cli.rb
+++ lib/test/cli/cli.rb
@@ -7,9 +7,7 @@ class disney_rides::CLI
   end   
   
   def list_rides 
-    
     puts "The Top 10 Rides at Disney World:"
-   
     @rides = Disney_Rides::Ride.today
   end 
   
@@ -18,18 +16,17 @@ class disney_rides::CLI
     while != "exit"
         puts "Enter the number to know what ride is ranked or type list to see the ride names again or type exit to enter :"
     input = gets.strip.downcase
-    case input 
-    when "1"
-      puts "Ride description" 
-    when "2"
-      puts "Ride description" 
-    when "list"
-      list_rides 
+    
+    if input.to_i > 0 
+      puts @rides [input.to_i-1]
+    elsif input == "list"  
+    list_rides
     else 
       puts "Not sure what you want, type list or exit" 
      end  
     end 
   end 
+  
   def goodbye 
   puts "see you next time!" 
 end 
