0e275c86a34b612961aaef4fe98b8b8f4b763fab
Angelica  Thompson <i.thompson89@outlook.com>
2020-01-17 04:10:14 +0000

    cleanup

--- lib/cli/cli.rb
+++ lib/cli/cli.rb
@@ -2,7 +2,6 @@ class DisneyRides::CLI
   
   def call 
     list_rides 
-    #binding.pry
     menu
     goodbye 
   end   
@@ -10,38 +9,34 @@ class DisneyRides::CLI
   def list_rides 
     puts "The Top 10 Rides at Disney World:"
     @rides = DisneyRides::Ride.scrape_tripsavvy
-    #binding.pry
     @rides.name.each.with_index(1) do |ride, i|
       puts "#{i}. #{ride}"
-      # #{ride.description} 
-    end
-    # binding.pry
+  end
     menu
   end 
   
   def menu 
-  input = nil 
-    while input != "exit"
+    input = nil 
+      while input != "exit"
         puts "Enter the number to know what ride is ranked or type list to see the ride names again or type exit to exit :"
     input = gets.strip.downcase
     
-    if input.to_i > 0 && input.to_i < 11
-      #the_ride = 
+      if input.to_i > 0 && input.to_i < 11
       puts @rides.name[input.to_i-1]
       puts "-------------"
       puts @rides.description[input.to_i-1]
-    elsif input == "list"  
-    list_rides
-    elsif input == "exit"
-    goodbye 
-    exit 
-    else 
-      puts "Not sure what you want, type number or exit" 
+        elsif input == "list"  
+          list_rides
+        elsif input == "exit"
+           goodbye 
+       exit 
+      else 
+        puts "Not sure what you want, type number or exit" 
      end  
     end 
   end 
   
   def goodbye 
-  puts "see you next time!" 
-end 
+   puts "see you next time!" 
+  end 
 end   
